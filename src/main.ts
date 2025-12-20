7f36490671032c32d2bdf8eafa1825725ea710c2
João Felipe Zini <joaofelipezini@gmail.com>
2025-12-20 16:35:31 -0300

    Add global validation pipe and improve main bootstrap
    Introduced a global ValidationPipe with whitelist and forbidNonWhitelisted options to enhance request validation. Added comments for clarity and improved structure in the main bootstrap function.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
-import { VersioningType } from '@nestjs/common'
+import { ValidationPipe, VersioningType } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
+  // Create the NestJS application instance
   const app = await NestFactory.create(AppModule)
+
+  // Enable URI versioning
   app.enableVersioning({
     type: VersioningType.URI,
   })
 
+  // Configure Swagger API documentation
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
     .setDescription('API documentation for NestJS - Mensageria application')
@@ -18,6 +22,16 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('docs', app, document)
 
+  // Set up global validation pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: false,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  )
+
+  // Start listening on the defined port
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
