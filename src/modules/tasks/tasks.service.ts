97719f731e0bb93421871b9260ada18f4674cc3a
João Felipe Zini <joaofelipezini@gmail.com>
2025-12-20 23:37:33 -0300

    Implement CRUD methods in TasksService
    Added methods to TasksService for finding, creating, updating, and deleting tasks by projectId using Prisma. This enhances the service with full CRUD functionality for task management.

--- src/modules/tasks/tasks.service.ts
+++ src/modules/tasks/tasks.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { TaskRequestDTO } from './tasks.dto'
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  findAllByProjectId(projectId: string) {
+    return this.prisma.task.findMany({ where: { projectId } })
+  }
+
+  findById(id: string, projectId: string) {
+    return this.prisma.task.findUnique({ where: { id, projectId } })
+  }
+
+  create(projectId: string, data: TaskRequestDTO) {
+    return this.prisma.task.create({ data: { ...data, projectId } })
+  }
+
+  update(id: string, projectId: string, data: TaskRequestDTO) {
+    const dataWithoutProjectId = { ...data, projectId }
+    return this.prisma.task.update({ where: { id, projectId }, data: dataWithoutProjectId })
+  }
+
+  remove(id: string, projectId: string) {
+    return this.prisma.task.delete({ where: { id, projectId } })
+  }
+}
