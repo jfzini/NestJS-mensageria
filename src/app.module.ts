5e46db0cc4762f936c0c45549b023b3ede84f1fd
João Felipe Zini <joaofelipezini@gmail.com>
2025-12-20 23:37:23 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ProjectsModule } from './modules/projects/projects.module'
+import { TasksModule } from './modules/tasks/tasks.module'
 import { PrismaService } from './prisma.service'
-import { TasksModule } from './modules/tasks/tasks.module';
 
 @Module({
   imports: [ProjectsModule, TasksModule],
