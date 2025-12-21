fa75bcc620afdb8750c1654be6cb722896f7904c
João Felipe Zini <joaofelipezini@gmail.com>
2025-12-20 23:37:38 -0300

    Implement tasks controller with CRUD endpoints
    Added TasksController with endpoints for listing, retrieving, creating, updating, and deleting tasks within a project. Integrated validation, error handling, and Swagger API responses for each route.

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -1,4 +1,77 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common'
+import { ApiResponse } from '@nestjs/swagger'
+import { TaskListItemDTO, TaskRequestDTO } from './tasks.dto'
+import { TasksService } from './tasks.service'
 
-@Controller('tasks')
-export class TasksController {}
+@Controller({
+  path: 'projects/:projectId/tasks',
+  version: '1',
+})
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Get()
+  @ApiResponse({ status: HttpStatus.OK, type: [TaskListItemDTO] })
+  findAllByProjectId(@Param('projectId', ParseUUIDPipe) projectId: string) {
+    return this.tasksService.findAllByProjectId(projectId)
+  }
+
+  @Get(':id')
+  @ApiResponse({ status: HttpStatus.OK, type: TaskListItemDTO })
+  async findById(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Param('projectId', ParseUUIDPipe) projectId: string,
+  ) {
+    const foundTask = await this.tasksService.findById(id, projectId)
+    if (!foundTask) {
+      throw new HttpException('Task not found', HttpStatus.NOT_FOUND)
+    }
+    return foundTask
+  }
+
+  @Post()
+  @ApiResponse({ status: HttpStatus.CREATED, type: TaskListItemDTO })
+  async create(@Param('projectId', ParseUUIDPipe) projectId: string, @Body() data: TaskRequestDTO) {
+    return this.tasksService.create(projectId, data)
+  }
+
+  @Put(':id')
+  @ApiResponse({ status: HttpStatus.OK, type: TaskListItemDTO })
+  async update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Param('projectId', ParseUUIDPipe) projectId: string,
+    @Body() data: TaskRequestDTO,
+  ) {
+    const foundTask = await this.tasksService.findById(id, projectId)
+    if (!foundTask) {
+      throw new HttpException('Task not found', HttpStatus.NOT_FOUND)
+    }
+    return this.tasksService.update(id, projectId, data)
+  }
+
+  @Delete(':id')
+  @ApiResponse({ status: HttpStatus.NO_CONTENT })
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Param('projectId', ParseUUIDPipe) projectId: string,
+  ) {
+    const foundTask = await this.tasksService.findById(id, projectId)
+    if (!foundTask) {
+      throw new HttpException('Task not found', HttpStatus.NOT_FOUND)
+    }
+    return this.tasksService.remove(id, projectId)
+  }
+}
