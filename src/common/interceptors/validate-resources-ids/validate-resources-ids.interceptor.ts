bd91a987c551e2a8686282ff17cf04c1711cdd1a
João Felipe Zini <joaofelipezini@gmail.com>
2025-12-25 11:54:58 -0300

    Add ValidateResourcesIdsInterceptor and tests
    Introduces ValidateResourcesIdsInterceptor to validate project IDs in requests using Prisma. Includes a basic unit test to ensure the interceptor is defined.

---
+++ src/common/interceptors/validate-resources-ids/validate-resources-ids.interceptor.ts
@@ -0,0 +1,43 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
+import { Observable } from 'rxjs'
+import { VALIDATE_RESOURCES_IDS_KEY } from 'src/consts'
+import { PrismaService } from 'src/prisma.service'
+
+@Injectable()
+export class ValidateResourcesIdsInterceptor implements NestInterceptor {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly prisma: PrismaService,
+  ) {}
+
+  async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<Request>> {
+    // Valida se o endpoint possui o decorator que indica a necessidade de validação
+    const shouldValidate = this.reflector.get<boolean>(
+      VALIDATE_RESOURCES_IDS_KEY,
+      context.getHandler(),
+    )
+
+    if (!shouldValidate) {
+      return next.handle()
+    }
+
+    // Valida o ProjectId
+    const request = context.switchToHttp().getRequest()
+    const projectId = request.params.id
+
+    const project = await this.prisma.project.findUnique({ where: { id: projectId } })
+    if (!project) {
+      throw new HttpException('Project not found', HttpStatus.NOT_FOUND)
+    }
+
+    return next.handle()
+  }
+}
